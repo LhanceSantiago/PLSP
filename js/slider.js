990ddf2eb951ad74d00f062544521a929ebb6a14
lans <139159115+772nacho@users.noreply.github.com>
2024-11-19 23:31:44 +0800

    first

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+var slider = tns({
+    container: '.blog-3__slider',
+    items: 3,
+    slideBy: 'page',
+    autoplay: true,
+    controls: false,
+    mouseDrag: true,
+    autoplayButtonOutput: false,
+    autoplayTimeout: 5000,
+    nav: false,
+    loop: true,
+    controlsContainer: ".blog-3__slider--controls",
+    controls: true,
+    gutter: 20
+  });
